d5ac76f11eb05240b0dde6ab50b74da69b45f592
bhumur-rough <agrawalbhumur@gmail.com>
2025-06-23 21:22:29 +0530

    day1continue

---
+++ Day1/ClassCode/Program.cs
@@ -0,0 +1,25 @@
+﻿using one;
+
+namespace ClassCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            one.Hello obj1 = new one.Hello();
+            obj1.show();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+namespace one
+{
+    public class  Hello()
+    {
+        public void show()
+        {
+            Console.WriteLine("i am form one");
+        }
+    }
+}
